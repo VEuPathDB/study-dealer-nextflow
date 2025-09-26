11af47d2a6f467756a40fbc349d00022ada3b780
John Brestelli <jbrestell@gmail.com>
2025-09-26 16:20:33 -0400

    performance improvement for reading counts; other rnaseq specific things

--- bin/wrangleRNASeq.R
+++ bin/wrangleRNASeq.R
@@ -9,14 +9,14 @@ library(study.wrangler)
 benchmark_results <- list()
 
 benchmark <- function(name, expr) {
-  cat("Benchmarking:", name, "...\n")
+  cat("Running:", name, "...\n")
   result <- system.time({
     value <- force(expr)
   })
   benchmark_results[[name]] <<- result
-  cat("  Elapsed time:", result["elapsed"], "seconds\n")
-  cat("  User time:", result["user"], "seconds\n")
-  cat("  System time:", result["sys"], "seconds\n\n")
+  ## cat("  Elapsed time:", result["elapsed"], "seconds\n")
+  ## cat("  User time:", result["user"], "seconds\n")
+  ## cat("  System time:", result["system"], "seconds\n\n")
   return(value)
 }
 
@@ -33,55 +33,58 @@ print_benchmark_summary <- function() {
 }
 
 
+read_wgcna_data <- function(filename) {
+  col_specs <- cols(
+    .default = '?', # guess
+    "...1" = col_character()
+  )
+
+  data <- read_rnaseq_data_default(filename, col_specs) %>%
+    mutate(wgcna.ID = sample.ID) %>%
+    relocate(wgcna.ID, .after = sample.ID) %>%
+    rename(assay.ID = sample.ID)
+
+  
+  return(data)
+}
+
 read_rnaseq_data <- function(filename) {
+
+  col_specs <- cols(
+    .default = col_integer(),
+    "...1" = col_character()
+  )
+
+  data <- read_rnaseq_data_default(filename, col_specs) %>%
+    mutate(assay.ID = sample.ID) %>%
+    relocate(assay.ID, .after = sample.ID)
+
+
+  return(data)
+}
+
+
+read_rnaseq_data_default <- function(filename, columnSpec) {
   cat("Reading RNA-seq data from:", basename(filename), "\n")
 
-  # read in as all-character
+
+ # read in as all-character
   data <- benchmark(paste("read_tsv", basename(filename)), {
     read_tsv(
       filename,
-      col_names = FALSE,
-      col_types = cols(.default = "c")
+      col_names = TRUE,
+      col_types= col_specs
     )
   })
 
-  # and transpose
-  data <- benchmark(paste("transpose", basename(filename)), {
-    data %>%
-      as.matrix() %>%
-      t()
-  })
-
-  # make colnames manually to avoid pairwise compute
-  colnames(data) <- sprintf("V%i", seq_len(ncol(data)))
-
-  # wrap it in a tibble with NO name repair overhead
-  data <- benchmark(paste("as_tibble", basename(filename)), {
-    as_tibble(data, .name_repair = "minimal")
-  })
-
-  # Extract the header row
-  headers <- data %>% slice_head(n = 1) %>% unlist(use.names = FALSE)
+  headers <- colnames(data)
   headers[1] <- 'sample.ID'
-  # Drop header row
-  data <- data %>% slice_tail(n = nrow(data) - 1)
-  # Name the columns and we're back to a proper tibble
   colnames(data) <- headers
 
-  # convert the counts columns to integer
-  # and make a trivial assay ID column
-  data <- benchmark(paste("type_conversion", basename(filename)), {
-    data %>%
-      mutate(
-        across(
-          -sample.ID,
-          as.integer
-        ),
-        assay.ID = sample.ID
-      ) %>%
-      relocate(assay.ID, .after = sample.ID) %>%
-      select(-starts_with('__'))
-  })
+  data <- data %>%
+    pivot_longer(-sample.ID, names_to = "column_name", values_to = "value") %>%
+    pivot_wider(names_from = sample.ID, values_from = value) %>%
+    rename(sample.ID = column_name)
 
   return(data)
 }
@@ -97,17 +100,17 @@ rename_counts_by_strandedness <- function(counts_list, orgAbbrev) {
     names(counts_list) <- paste0("unstranded_", orgAbbrev)
     return(counts_list)
   }
-  
+
   stopifnot(length(counts_list) == 2)
-  
+
   # Extract tibbles
   x <- counts_list[[1]]
   y <- counts_list[[2]]
-  
+
   # Remove metadata columns (assumes first 2 cols are not genes)
   x_counts <- x %>% select(-1, -2)
   y_counts <- y %>% select(-1, -2)
-  
+
   # Compute per-sample sums (across each row)
   x_sums <- rowSums(x_counts)
   y_sums <- rowSums(y_counts)
@@ -122,78 +125,85 @@ rename_counts_by_strandedness <- function(counts_list, orgAbbrev) {
 
   prefix = paste0(orgAbbrev, "_")
 
-  names(counts_list) = paste0(prefix, names(counts_list)) 
-  
+  names(counts_list) = paste0(prefix, names(counts_list))
+
   return(counts_list);
 }
 
 counts_to_entity <- function(tbl, name, orgAbbrev) {
 
   # strip off the prefix
-  name = sub(glue("{orgAbbrev}_"), "", name); 
-  
-  assays <- entity_from_tibble(
-    tbl,
-    name = glue("{orgAbbrev}_{name}_counts"),
-    display_name = glue("{orgAbbrev} {name} htseq counts"),
-    display_name_plural = glue("{orgAbbrev} {name} htseq counts"),
-    skip_type_convert = TRUE
-  ) %>%
-    set_parents('sample', 'sample.ID') %>% 
-    set_variable_display_names_from_provider_labels() %>%
-    set_variable_metadata('sample.ID', display_name = "Sample ID", hidden=list('variableTree')) %>%
-    set_variable_metadata('assay.ID', display_name = "HTSeq Count", hidden=list('variableTree'))
-    
-  
-  assays <- assays %>%
-    create_variable_category(
-      category_name = 'gene_counts',
-      children = assays %>% get_variable_metadata() %>% pull(variable),
-      display_name = 'Gene counts',
-      definition = 'Counts per gene from RNA-Seq'
+  name = sub(glue("{orgAbbrev}_"), "", name);
+
+  assays <- benchmark(paste("entity_from_tibble", name), {
+    entity_from_tibble(
+      tbl,
+      name = glue("{orgAbbrev}_{name}_counts"),
+      display_name = glue("{orgAbbrev} {name} htseq counts"),
+      display_name_plural = glue("{orgAbbrev} {name} htseq counts"),
+      skip_type_convert = TRUE
     ) %>%
-    create_variable_collection(
-      'gene_counts',
-      member = 'gene',
-      member_plural = 'genes',
-      is_proportion = FALSE,
-      is_compositional = TRUE
-    )
+      set_parents('sample', 'sample.ID') %>%
+      set_variable_display_names_from_provider_labels() %>%
+      set_variable_metadata('sample.ID', display_name = "Sample ID", hidden=list('variableTree')) %>%
+      set_variable_metadata('assay.ID', display_name = "HTSeq Count", hidden=list('variableTree'))
+  })
+
+  assays <- benchmark(paste("create_variable_metadata", name), {
+    assays %>%
+      create_variable_category(
+        category_name = 'gene_counts',
+        children = assays %>% get_variable_metadata() %>% pull(variable),
+        display_name = 'Gene counts',
+        definition = 'Counts per gene from RNA-Seq'
+      ) %>%
+      create_variable_collection(
+        'gene_counts',
+        member = 'gene',
+        member_plural = 'genes',
+        is_proportion = FALSE,
+        is_compositional = TRUE
+      )
+  })
 
   assays
 }
 
 
 wgcna_to_entity <- function(tbl, name, orgAbbrev) {
-
-  assays <- entity_from_tibble(
-    tbl,
-    name = glue("{orgAbbrev}_eigengene"),
-    display_name = glue("{orgAbbrev} Eigengene (wgcna)"),
-    display_name_plural = glue("{orgAbbrev} Eigengenes (wgcna)"),
-    skip_type_convert = TRUE
-    #TO DO, description = ???
-  ) %>%
-    set_parents(glue("{orgAbbrev}_{name}_counts"), 'assay.ID') %>%
-    set_variable_display_names_from_provider_labels() %>%
-    set_variable_metadata('sample.ID', display_name = "Sample ID", hidden=list('variableTree')) %>%
-    set_variable_metadata('assay.ID', display_name = "Assay ID", hidden=list('variableTree'))
-
-  assays <- assays %>%
-    create_variable_category(
-      category_name = 'wgcna',
-      children = assays %>% get_variable_metadata() %>% pull(variable),
-      display_name = 'Eigengenes',
-      definition = 'Eigengene from RNA-Seq'
+  
+  assays <- benchmark(paste("wgcna_entity_from_tibble", name), {
+    entity_from_tibble(
+      tbl,
+      name = glue("{orgAbbrev}_eigengene"),
+      display_name = glue("{orgAbbrev} Eigengene (wgcna)"),
+      display_name_plural = glue("{orgAbbrev} Eigengenes (wgcna)"),
+      skip_type_convert = TRUE
+      #TO DO, description = ???
     ) %>%
-    create_variable_collection(
-      'wgcna',
-      member = 'eigengene',
-      member_plural = 'eigengenes',
-      stable_id =  "EUPATH_0005051",
-      is_proportion =  FALSE,
-      is_compositional = FALSE
-    )
+      set_parents(glue("{orgAbbrev}_Sense_counts"), 'assay.ID') %>%
+      set_variable_display_names_from_provider_labels() %>%
+      set_variable_metadata('assay.ID', display_name = "Assay ID", hidden=list('variableTree')) %>%
+      set_variable_metadata('wgcna.ID', display_name = "WGCNA ID", hidden=list('variableTree'))
+  })
+
+  assays <- benchmark(paste("wgcna_variable_metadata", name), {
+    assays %>%
+      create_variable_category(
+        category_name = 'wgcna',
+        children = assays %>% get_variable_metadata() %>% pull(variable),
+        display_name = 'Eigengenes',
+        definition = 'Eigengene from RNA-Seq'
+      ) %>%
+      create_variable_collection(
+        'wgcna',
+        member = 'eigengene',
+        member_plural = 'eigengenes',
+        stable_id =  "EUPATH_0005051",
+        is_proportion =  FALSE,
+        is_compositional = FALSE
+      )
+  })
 
   assays
 }
@@ -213,81 +223,114 @@ wrangle <- function() {
 
   countsPattern = "_countsForEda_"
   wgcnaPattern = '_merged-0.25-eigengenes_'
-  
+
   # find the sample STF file
   sample_tsv_path <- file.path(
     '*entity-sample.tsv'
   )
 
-  sample_tsv_file = Sys.glob(sample_tsv_path)
-  
-  
-  # find the countsForEda_*.txt files
-  counts_file_glob <- file.path(
-    glue("*{countsPattern}*")
-  )
+  file_discovery <- benchmark("file_globbing", {
+    sample_tsv_file = Sys.glob(sample_tsv_path)
 
-  counts_filenames <- Sys.glob(counts_file_glob)
-  count_files_by_prefix <- group_files_by_prefix(counts_filenames, countsPattern)
+    # find the countsForEda_*.txt files
+    counts_file_glob <- file.path(
+      glue("*{countsPattern}*")
+    )
 
-  # find the countsForEda_*.txt files
-  wgcna_file_glob <- file.path(
-    glue("*{wgcnaPattern}*")
-  
-  )
+    counts_filenames <- Sys.glob(counts_file_glob)
+    count_files_by_prefix <- group_files_by_prefix(counts_filenames, countsPattern)
 
-  wgcna_filenames <- Sys.glob(wgcna_file_glob)
-  wgcna_files_by_prefix <- group_files_by_prefix(wgcna_filenames, wgcnaPattern)
+    # find the countsForEda_*.txt files
+    wgcna_file_glob <- file.path(
+      glue("*{wgcnaPattern}*")
+    )
 
-  # call countsData() on each group, passing (files, prefix)  
-  all_counts_entities <- count_files_by_prefix %>%
-    imap(~ countsData(.x, .y)) %>%
-    flatten()
+    wgcna_filenames <- Sys.glob(wgcna_file_glob)
+    wgcna_files_by_prefix <- group_files_by_prefix(wgcna_filenames, wgcnaPattern)
 
+    list(
+      sample_tsv_file = sample_tsv_file,
+      count_files_by_prefix = count_files_by_prefix,
+      wgcna_files_by_prefix = wgcna_files_by_prefix
+    )
+  })
 
-  # call countsData() on each group, passing (files, prefix)  
-  all_wgcna_entities <- wgcna_files_by_prefix %>%
-    imap(~ wgcnaData(.x, .y)) %>%
-    flatten()
+  # call countsData() on each group, passing (files, prefix)
+  all_counts_entities <- benchmark("process_counts_data", {
+    file_discovery$count_files_by_prefix %>%
+      imap(~ countsData(.x, .y)) %>%
+      flatten()
+  })
 
-  
+  # call wgcnaData() on each group, passing (files, prefix)
+  all_wgcna_entities <- benchmark("process_wgcna_data", {
+    file_discovery$wgcna_files_by_prefix %>%
+      imap(~ wgcnaData(.x, .y)) %>%
+      flatten()
+  })
+
   # the entity will have the name 'sample'
-  samples <- entity_from_stf(sample_tsv_file) %>%
-    set_variable_metadata('sample.ID', display_name = "Sample ID", hidden=list('variableTree'))
-  
-  study <- study_from_entities(c(samples, all_counts_entities, all_wgcna_entities), name = "RNA-Seq study")
-  
+  samples <- benchmark("process_sample_metadata", {
+    entity_from_stf(file_discovery$sample_tsv_file) %>%
+      set_variable_metadata('sample.ID', display_name = "Sample ID", hidden=list('variableTree'))
+  })
+
+  study <- benchmark("create_study", {
+    study_from_entities(c(samples, all_counts_entities, all_wgcna_entities), name = "RNA-Seq study")
+  })
+
+  print_benchmark_summary()
+
   study
 }
 
 
 countsData <- function(counts_filenames, orgAbbrev) {
 
-  counts_data <- counts_filenames %>%
-    set_names() %>%
-    map(read_rnaseq_data)  %>%
-    rename_counts_by_strandedness(., orgAbbrev) %>%
-    imap(counts_to_entity, orgAbbrev)
+  counts_data = #<- benchmark(paste("read_counts_files", orgAbbrev), {
+    counts_filenames %>%
+      set_names() %>%
+      map(read_rnaseq_data)
+  #})
+
+  
+  counts_data <- benchmark(paste("rename_strandedness", orgAbbrev), {
+    rename_counts_by_strandedness(counts_data, orgAbbrev)
+  })
+
+  counts_data <- benchmark(paste("convert_to_entities", orgAbbrev), {
+    counts_data %>%
+      imap(counts_to_entity, orgAbbrev)
+  })
 
   return(counts_data)
 }
 
 wgcnaData <- function(counts_filenames, orgAbbrev) {
 
-  counts_data <- counts_filenames %>%
-    set_names() %>%
-    map(read_rnaseq_data)  %>%
-    imap(wgcna_to_entity, orgAbbrev)
-    #imap(purrr::partial(wgcna_to_entity, orgAbbrev=orgAbbrev))
+  counts_data <- benchmark(paste("read_wgcna_files", orgAbbrev), {
+    counts_filenames %>%
+      set_names() %>%
+      map(read_wgcna_data)
+  })
+
+  counts_data <- benchmark(paste("convert_wgcna_to_entities", orgAbbrev), {
+    counts_data %>%
+      imap(wgcna_to_entity, orgAbbrev)
+  })
 
   return(counts_data)
 }
 
 
 
-## study = wrangle()
-## if(!validate(study, profiles=c("baseline", "eda")) {
-##   stop("Stopping....Study is not valid");
-## }
+# Run the wrangling with benchmarking
+#study = wrangle()
 
-## export_to_vdi(study, getwd());
+# Validate the study (uncomment if validation is needed)
+# if(!validate(study, profiles=c("baseline", "eda")) {
+#   stop("Stopping....Study is not valid");
+# }
+
+# Export to VDI (uncomment if export is needed)
+# export_to_vdi(study, getwd());
